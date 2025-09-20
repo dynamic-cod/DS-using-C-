cc6570373ccb3a163fa75870ebed552771cdff39
Asif Khan <136554084+dynamic-cod@users.noreply.github.com>
2025-09-20 23:14:02 +0530

    Create program to print name and details

---
+++ printName.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "Name: John Doe" << endl;
+    cout << "Age: 20" << endl;
+    cout << "City: New York" << endl;
+    return 0;
+}
