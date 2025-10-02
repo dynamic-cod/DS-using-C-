bc716dbc7c8288f5277c8a71e8bd87976c17626b
Asif Khan <136554084+dynamic-cod@users.noreply.github.com>
2025-10-02 23:32:54 +0530

    copying elements of an array

---
+++ array12.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr1[n], arr2[n];
+
+    cout << "Enter elements of first array: ";
+    for(int i = 0; i < n; i++) 
+      cin >> arr1[i];
+
+    // Copy elements
+    for(int i = 0; i < n; i++) 
+      arr2[i] = arr1[i];
+
+    cout << "Copied array: ";
+    for(int i = 0; i < n; i++) 
+      cout << arr2[i] << " ";
+    return 0;
+}
