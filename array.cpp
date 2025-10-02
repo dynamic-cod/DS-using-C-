1271f7ff80f0c3312dfcdca383d52e85ab278428
Asif Khan <136554084+dynamic-cod@users.noreply.github.com>
2025-10-02 22:38:27 +0530

    Create array.cpp
    program to input 5 integers into an array and display them

---
+++ array.cpp
@@ -0,0 +1,20 @@
+//program to input 5 integers into an array and display them
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+
+    // Input 5 integers
+    cout << "Enter 5 integers: ";
+    for(int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    // Display integers
+    cout << "Array elements are: ";
+    for(int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
