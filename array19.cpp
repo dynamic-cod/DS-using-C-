a12e0995aae58a5f30fc472352fbe0f30212b7f8
Asif Khan <136554084+dynamic-cod@users.noreply.github.com>
2025-10-03 00:23:31 +0530

    merging to arrays into a third array

---
+++ array19.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2;
+    cout << "Enter size of first array: ";
+    cin >> n1;
+    int arr1[n1];
+
+    cout << "Enter elements of first array: ";
+    for(int i = 0; i < n1; i++) 
+      cin >> arr1[i];
+
+    cout << "Enter size of second array: ";
+    cin >> n2;
+    int arr2[n2];
+
+    cout << "Enter elements of second array: ";
+    for(int i = 0; i < n2; i++) 
+      cin >> arr2[i];
+
+    int arr3[n1+n2];  //declaring array of size n1+n2;
+
+    // Merge arrays
+    for(int i = 0; i < n1; i++) 
+      arr3[i] = arr1[i];
+    for(int i = 0; i < n2; i++) 
+      arr3[n1+i] = arr2[i];  //here we are continuing coping elemnts of arr2[] into arr3 from the index n1
+
+    cout << "Merged array: ";
+    for(int i = 0; i < n1+n2; i++) //printing arr3 of size (n1+n2)
+      cout << arr3[i] << " ";
+    return 0;
+}
