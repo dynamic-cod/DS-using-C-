e54fb690a3376763e060371dc361414f82044ace
Asif Khan <136554084+dynamic-cod@users.noreply.github.com>
2025-10-02 23:37:42 +0530

    checking array is sorted in ascending order or not

---
+++ array14.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter elements in ascending order: ";
+    for(int i = 0; i < n; i++) 
+      cin >> arr[i];
+
+    bool sorted = true;
+    for(int i = 0; i < n-1; i++) {
+        if(arr[i] > arr[i+1]) {
+            sorted = false;
+            break;
+        }
+    }
+
+    if(sorted) 
+      cout << "Array is sorted in ascending order.";
+    else 
+      cout << "Array is not sorted.";
+    return 0;
+}
