49b1925464f6861e238bdc077c253cbbb447d53c
Asif Khan <136554084+dynamic-cod@users.noreply.github.com>
2025-10-10 00:39:59 +0530

    converting lowercase to uppercase

---
+++ string5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    for (int i = 0; str[i] != '\0'; i++)
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] -= 32;
+
+    cout << "Uppercase: " << str << endl;
+    return 0;
+}
